266456afda64e29aac6bec23b244fa14f00f9eac
Alexis <alexisporcelano@gmail.com>
2024-05-21 16:54:05 -0300

    chat connected to backend

--- cardGame/src/Components/chat/Chat.jsx
+++ cardGame/src/Components/chat/Chat.jsx
@@ -1,10 +1,56 @@
-import { Box } from "@mui/material";
+import { Box, TextField, Button } from "@mui/material";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function chat(){
-    return(
+export default function Chat() {
+
+    const [chat, setChat] = useState([]);
+    const [newChat, setNewChat] = useState("");
+
+    const sendChat = async (message) => {
+        try {
+            console.log('sending chat');
+            const response = await axios.post('http://localhost:3001/chat', { message });
+
+            const { data } = response;
+
+            if (data) {
+                console.log('chat sent', data);
+                setChat( data );
+                setNewChat(""); 
+            }
+        } catch (error) {
+            window.alert(`Error: ${error.message}`);
+        }
+    };
+
+    const handleInputChange = (event) => {
+        setNewChat(event.target.value);
+    };
+
+    useEffect(() => {
+        console.log(newChat);
+    }, [newChat])
+
+    return (
         <Box>
-            
+            <Box sx={{ border: '1px solid green', minHeight: 50 }}>
+                {chat.length > 0
+                    ? chat.map((message, i) => (
+                        <h5 key={i}>{message}</h5>
+                    ))
+                    : null}
+            </Box>
+            <Box sx={{ display: "flex", border: "1px solid red" }}>
+                <TextField 
+                    id="newChat" 
+                    label="Send message" 
+                    variant="standard" 
+                    value={newChat} 
+                    onChange={handleInputChange} 
+                />
+                <Button onClick={() => sendChat(newChat)}>Send</Button>
+            </Box>
         </Box>
-    )
+    );
 }
